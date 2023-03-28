30f140d487670cb4266625eeb5a275ead0e93072
Bewket <brishget58@gmail.com>
2023-03-28 11:38:17 -0700

    python classes and objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-    pass
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
