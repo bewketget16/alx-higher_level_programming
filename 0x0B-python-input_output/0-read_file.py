995ce1496471d2d0680df61deff8aac72593981c
Bewket <brishget58@gmail.com>
2023-04-11 02:51:45 -0700

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-
-
-"""Defines a text file-reading function."""
+""" Module that contains a function that reads from a file """
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """ Function that reads from a file
+    Args:
+        filename: filename
+    Raises
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
