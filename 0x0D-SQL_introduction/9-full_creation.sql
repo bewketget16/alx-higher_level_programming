7e7285a728dff3fa16ae2ac65cbd8f23f0880603
Bewket <brishget58@gmail.com>
2023-05-18 17:05:05 -0700

    updated

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Creates and fills a table second_table with attributes id, name and score.
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Alex", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Bemenet", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Chalia", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "Dani", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
