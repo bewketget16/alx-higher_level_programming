5c19d7782e30bac3bc81d9a72cf3509b459b7bc8
Bewket <brishget58@gmail.com>
2023-05-18 17:01:36 -0700

    tasks on sql introduction

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Creates and fills a table second_table with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Alex", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Bemenet", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Chalia", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "Dani", 8);
